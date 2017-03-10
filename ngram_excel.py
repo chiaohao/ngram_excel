eeb6d4c402b050f6aace1c9be012a0c2a0fed2be
chiaohao <ksjoey0096@gmail.com>
2017-03-10 14:46:28 +0800

    small fix

--- ngram_excel.py
+++ ngram_excel.py
@@ -7,10 +7,7 @@ import sys
 
 cutlist = "<>/:：;；,、＂’，.。！？「」（）｢\"\'\\\n\r《》“”!@#$%^&*()".decode("utf-8")
 
-def cutSentence(workbook_path, contentColumnIndex): 
-	in_workbook = xlrd.open_workbook(workbook_path)
-	text_column = in_workbook.sheets()[0].col_values(contentColumnIndex)
-	row_counts = in_workbook.sheets()[0].nrows
+def cutSentence(text_column): 
 	#print repr([a.encode(sys.stdout.encoding) for a in text_column]).decode("string-escape")
 	sentence = ""
 	textList = []
@@ -44,11 +41,11 @@ def ngram(textLists,n,minFreq):
 
 	return result
 
-def longTermPriority(path, maxTermLength, minFreq, contentColumnIndex):
+def longTermPriority(text_column, maxTermLength, minFreq):
 	longTermsFreq=[]
 	
 	for i in range(maxTermLength,1,-1):
-		text_list = cutSentence(path, contentColumnIndex)
+		text_list = cutSentence(text_column)
 		words_freq = ngram(text_list,i, minFreq)
 	
 		for word_freq in words_freq:
@@ -56,11 +53,7 @@ def longTermPriority(path, maxTermLength, minFreq, contentColumnIndex):
 	
 	return longTermsFreq
 
-def CountDocumentFrequency(workbook_path, gram_with_tf, contentColumnIndex):
-	in_workbook = xlrd.open_workbook(workbook_path)
-	text_column = in_workbook.sheets()[0].col_values(contentColumnIndex)
-	row_counts = in_workbook.sheets()[0].nrows
-
+def CountDocumentFrequency(text_column, gram_with_tf):
 	gram_tf_df = []
 
 	for gram in gram_with_tf:
@@ -77,19 +70,27 @@ def CountDocumentFrequency(workbook_path, gram_with_tf, contentColumnIndex):
 #python ngram_excel.py in_workbook content_column out_workbook longest_gram_num min_freqency
 #                      sys.argv[1] sys.argv[2]    sys.argv[3]  sys.argv[4]      sys.argv[5]
 
+
 col = ord(sys.argv[2].lower()) - ord('a')
 
-longTermFreq = longTermPriority(sys.argv[1], int(sys.argv[4]), int(sys.argv[5]), col)
-gram_tf_df = CountDocumentFrequency(sys.argv[1], longTermFreq, col)
+in_workbook = xlrd.open_workbook(sys.argv[1])
+text_column = in_workbook.sheets()[0].col_values(col)
+row_counts = in_workbook.sheets()[0].nrows
+
+longTermFreq = longTermPriority(text_column, int(sys.argv[4]), int(sys.argv[5]))
+gram_tf_df = CountDocumentFrequency(text_column, longTermFreq)
 
 out_workbook = xlwt.Workbook()
 out_table = out_workbook.add_sheet("output")
 
-out_table.write(0, 0, "gram")
-out_table.write(0, 1, "tf")
-out_table.write(0, 2, "df")
+out_table.write(0, 0, "docNum")
+out_table.write(0, 1, row_counts)
 
-c = 1
+out_table.write(2, 0, "gram")
+out_table.write(2, 1, "tf")
+out_table.write(2, 2, "df")
+
+c = 3
 for i in gram_tf_df:
 	out_table.write(c, 0, i[0])
 	out_table.write(c, 1, i[1])
