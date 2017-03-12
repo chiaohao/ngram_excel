45f477bfb484240a599f96339f220ecc331c6897
chiaohao <ksjoey0096@gmail.com>
2017-03-12 19:03:38 +0800

    small fix

--- ngram_excel.py
+++ ngram_excel.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-import xlrd
-import xlwt
 import codecs
 import operator
 import sys
+import xlrd
+import xlsxwriter
 
-cutlist = "<>/:：;；,、＂’，.。！？「」（）｢\"\'\\\n\r《》“”!@#$%^&*()".decode("utf-8")
+cutlist = " <>/:：;；,、＂’，.。！？「」（）｢\"\'\\\n\r《》“”!@#$%^&*()".decode("utf-8")
 
 def cutSentence(text_column): 
 	#print repr([a.encode(sys.stdout.encoding) for a in text_column]).decode("string-escape")
@@ -67,6 +67,9 @@ def CountDocumentFrequency(text_column, gram_with_tf):
 
 	return gram_tf_df
 
+def gramDF(gram, text_column):
+
+
 #python ngram_excel.py in_workbook content_column out_workbook longest_gram_num min_freqency
 #                      sys.argv[1] sys.argv[2]    sys.argv[3]  sys.argv[4]      sys.argv[5]
 
@@ -78,13 +81,15 @@ text_column = in_workbook.sheets()[0].col_values(col)
 row_counts = in_workbook.sheets()[0].nrows
 
 longTermFreq = longTermPriority(text_column, int(sys.argv[4]), int(sys.argv[5]))
+print "tf done"
 gram_tf_df = CountDocumentFrequency(text_column, longTermFreq)
+print "df done"
 
-out_workbook = xlwt.Workbook()
-out_table = out_workbook.add_sheet("output")
+out_workbook = xlsxwriter.Workbook(sys.argv[3])
+out_table = out_workbook.add_worksheet()
 
-out_table.write(0, 0, "docNum")
-out_table.write(0, 1, row_counts)
+out_table.write(0, 1, "docNum")
+out_table.write(0, 0, row_counts)
 
 out_table.write(2, 0, "gram")
 out_table.write(2, 1, "tf")
@@ -97,5 +102,4 @@ for i in gram_tf_df:
 	out_table.write(c, 2, i[2])
 	c = c + 1
 
-out_workbook.save(sys.argv[3])
-
+out_workbook.close()
